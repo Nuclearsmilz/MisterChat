2f1a58f781dbac206257e6940984646d9ca9517a
Jon Marien <nuclearsmilz@gmail.com>
2015-01-27 14:56:22 -0800

    Made the Client class, aka another window, and changed a lot of stuff in the Login class.

--- src/com/nuclearsmilz/misterchat/Login.java
+++ src/com/nuclearsmilz/misterchat/Login.java
@@ -1,19 +1,16 @@
 package com.nuclearsmilz.misterchat;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
 
 public class Login extends JFrame {
 
@@ -48,7 +45,6 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
-		
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
@@ -116,6 +112,7 @@ public class Login extends JFrame {
 	 */
 	private void login(String name, String address, int port) {
 		dispose();
-		System.out.println(name + ", " + address + ", " + port);
+		new Client(name, address, port);
+		//System.out.println(name + ", " + address + ", " + port);
 	}
 }
